e6384a96c933ee1f4007cc0445845865eecedad6
unknown <Admin@.(none)>
2011-09-13 23:46:31 +0300

    Initial commit

---
+++ ex1.rb
@@ -0,0 +1,7 @@
+#puts "Hello World!"
+#puts "Hello Again"
+#puts "I like typing this."
+puts "This is fun." "2." "3" "4.""5" '6''7'            '8'
+#puts 'Yay! Printing.'
+#puts "I'd much rather you 'not'."
+#puts 'I "said" do not touch this.'
